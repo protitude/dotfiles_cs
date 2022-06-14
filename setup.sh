ab037bbeaa690854c8acc1b3a8f7204c0ce95568
protitude <miles.france@gmail.com>
2022-06-13 22:39:35 -0600

    adding in some more setup items - removing .zshrc for now

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-files=( ".tmux-git" ".tmux-git.conf" ".tmux.conf" ".tmux.theme.sh" ".bash_profile" ".gitconfig" ".gitignore" ".vim" ".vimrc" ".tmux.conf" ".goto_shortcuts" ".zshrc")
+sudo apt install vim nodejs npm tmux -y
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+wget -P /tmp https://starship.rs/install.sh
+chmod u+x /tmp/install.sh
+/tmp/install.sh -y
+
+files=( ".tmux-git" ".tmux-git.conf" ".tmux.conf" ".tmux.theme.sh" ".bash_profile" ".gitconfig" ".gitignore" ".vim" ".vimrc" ".tmux.conf" ".goto_shortcuts" )
 
 for i in "${files[@]}"
 do
