ff0672b126f73cf49f87ba54ec034680e30999a1
protitude <miles.france@gmail.com>
2022-08-27 10:35:42 -0600

    throw more into cs only

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ if [[ $1 == "linode" ]]; then
 else
   type = 'cs'
 fi
-sudo apt update
-sudo apt install python3-dev python3-pip python3-setuptools -y
-sudo apt install vim nodejs npm tmux -y
-ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 if [[ $type == 'cs' ]]; then
+  sudo apt update
+  sudo apt install python3-dev python3-pip python3-setuptools -y
+  sudo apt install vim nodejs npm tmux -y
+  ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+  git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
   wget -P /tmp https://starship.rs/install.sh
   chmod u+x /tmp/install.sh
   /tmp/install.sh -y
